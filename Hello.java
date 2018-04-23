966f05fcdb0e003732501d1ee8be9148e3912291
andsven chen <1498704501@qq.com>
2018-04-23 16:50:20 +0800

    Add Hello.java,fix README.md

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello{
+
+
+	private String name;
+	private int age;
+
+	public Hello(){
+	}
+	public Hello(String name,int age){
+		this.name=name;
+		this.age=age;
+	}
+	public void say(){
+		System.out.println("hello");
+	}
+
